99b404a7353da3352a96f641d68589fe8731052f
Alex M <57906851+AlexMasood@users.noreply.github.com>
2021-08-23 23:04:36 +0100

    text edit

--- game.py
+++ game.py
@@ -28,11 +28,11 @@ class Game:
 
 
     def playerMove(self,board,player):
-        row = int(input("enter row"))
-        col = int(input("enter column"))
+        row = int(input("enter row: "))
+        col = int(input("enter column: "))
         while((0 <= row <= 2) and (0 <= col <= 2) and b.checkMove(board,row,col) != True):
-            row = int(input("enter row between or equal to 0 and 2"))
-            col = int(input("enter column between or equal to 0 and 2"))
+            row = int(input("enter row between or equal to 0 and 2: "))
+            col = int(input("enter column between or equal to 0 and 2: "))
         
         print(b.checkMove(board,row,col))
         if(b.checkMove(board,row,col) == True):
