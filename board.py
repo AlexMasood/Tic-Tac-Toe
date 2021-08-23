8904e899726acdd16004b05fda3db2027221962a
Alex M <57906851+AlexMasood@users.noreply.github.com>
2021-08-23 23:01:14 +0100

    Add files via upload

---
+++ board.py
@@ -0,0 +1,43 @@
+import numpy as np
+class Board:
+    def createBoard():
+        board = np.array([[0, 0, 0],[0, 0, 0],[0, 0, 0]])
+        return board
+    
+    def move(board,player,rowNum,colNum):
+        if(Board.checkMove(board,rowNum,colNum)):
+            board[rowNum][colNum] = player
+            return board
+        else:
+            return 0
+
+    def checkMove(board,rowNum,colNum):
+        if(board[rowNum][colNum] == 0):
+            return True
+        else:
+            return False
+    
+    def printBoard(board):
+        print(board)
+        
+    
+    def checkRow(row, player):
+        for index in row:
+            if (row[index] != player):
+                row[index] = 0
+        if(all(row)):
+            return True
+        else:
+            return False
+    
+    def checkBoard(board, player):
+        boardCopy = np.copy(board)
+        for x in range(0,2):
+            for row in boardCopy:
+                if (Board.checkRow(row,player)):
+                    return True
+            diag = [boardCopy[0][0],boardCopy[1][1],boardCopy[2][2]]
+            if (Board.checkRow(diag,player)):
+                return True
+            boardCopy = np.rot90(boardCopy)
+                    
---
+++ game.py
@@ -0,0 +1,43 @@
+from board import Board as b
+import numpy as np
+class Game:
+    def __init__(self):
+        self.beingPlayed = True
+    
+    
+    def GameLoop(self):
+        turn = 0
+        board = b.createBoard()
+        b.printBoard(board)
+        while (self.beingPlayed):
+            board = self.playerMove(board,1)
+            turn += 1
+            if(b.checkBoard(board, 1)):
+                print("player 1 has won")
+                self.beingPlayed = False
+            else:
+                if(turn > 8):
+                    print("draw")
+                    self.beingPlayed = False
+                else:
+                    board = self.playerMove(board,2)
+                    turn += 1
+                    if(b.checkBoard(board, 2)):
+                        print("player 2 has won")
+                        self.beingPlayed = False
+
+
+    def playerMove(self,board,player):
+        row = int(input("enter row"))
+        col = int(input("enter column"))
+        while((0 <= row <= 2) and (0 <= col <= 2) and b.checkMove(board,row,col) != True):
+            row = int(input("enter row between or equal to 0 and 2"))
+            col = int(input("enter column between or equal to 0 and 2"))
+        
+        print(b.checkMove(board,row,col))
+        if(b.checkMove(board,row,col) == True):
+            board = b.move(board,player,row,col)
+        b.printBoard(board)
+        return board
+g = Game()
+g.GameLoop()
