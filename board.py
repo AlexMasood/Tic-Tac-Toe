d692a11801fbb52cd44c61e8546404071a202b79
Alex M <57906851+AlexMasood@users.noreply.github.com>
2021-08-24 17:05:42 +0100

    Validation and comments
    Added validation to ensure the given inputs are within the correct range.
    Added validation to ensure given inputs are not allready placed moves.
    Added comments to functions

--- board.py
+++ board.py
@@ -4,6 +4,11 @@ class Board:
         board = np.array([[0, 0, 0],[0, 0, 0],[0, 0, 0]])
         return board
     
+    """
+    input of the board 3 by 3 numpy array, player int, row number int, column number int
+    checks if move is legal and sets the new board as its output
+    returns 0 if check fails
+    """
     def move(board,player,rowNum,colNum):
         if(Board.checkMove(board,rowNum,colNum)):
             board[rowNum][colNum] = player
@@ -11,6 +16,11 @@ class Board:
         else:
             return 0
 
+    """
+    input of the board 3 by 3 numpy array, row number int, row column int
+    checks if placing a move in the given coordinates are legal
+    returns true if legal, false if not
+    """
     def checkMove(board,rowNum,colNum):
         if(board[rowNum][colNum] == 0):
             return True
@@ -20,7 +30,12 @@ class Board:
     def printBoard(board):
         print(board)
         
-    
+    """
+    input of the row 3 element array, player int
+    for each element the element is set to 0 if it isnt the players move, this is to ignore empty coords and the other players moves
+    checks if the list contains 3 of the players move
+    returns true if check passes, else false
+    """
     def checkRow(row, player):
         for index in range(0,2):
             if (row[index] != player):#potentially add a check to see if row[index] isnt 0, may be faster than assignment
@@ -30,6 +45,15 @@ class Board:
         else:
             return False
     
+    """
+    input of board 3 by 3 numpy array, player int
+    creates a copy of the board
+    loops over each row to check if win condition has been met
+    checks if diagonal win condition has been met
+    rotates the board by 90 degrees
+    repeats the loop
+    returns true if win condition has been met
+    """
     def checkBoard(board, player):
         boardCopy = np.copy(board)
         for x in range(0,2):
--- game.py
+++ game.py
@@ -4,7 +4,14 @@ class Game:
     def __init__(self):
         self.beingPlayed = True
     
-    
+    """
+    whilst the game is being played,
+    player 1 move
+    check win condition
+    else check draw condition (turn>8 without a win)
+    else play player 2
+    check win condition
+    """
     def GameLoop(self):
         turn = 0
         board = b.createBoard()
@@ -26,11 +33,19 @@ class Game:
                         print("Player 2 has won")
                         self.beingPlayed = False
 
-
+    """
+    input of the current board and which players move it is
+    asks for the row and column from the player
+    validates that its a legal move and repeats the input if it is not
+    inputs the players choice into the board
+    prints the new board to terminal
+    outputs the board
+    """
     def playerMove(self,board,player):
         row = int(input("Enter row: "))
         col = int(input("Enter column: "))
-        while((0 <= row <= 2) and (0 <= col <= 2) and b.checkMove(board,row,col) != True):
+        while(not(0 <= row <= 2) or not(0 <= col <= 2) or b.checkMove(board,row,col) != True):
+            print("Make sure the coordinate hasn't been previously used.")
             row = int(input("Enter row between or equal to 0 and 2: "))
             col = int(input("Enter column between or equal to 0 and 2: "))
         if(b.checkMove(board,row,col) == True):
