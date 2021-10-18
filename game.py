6d0184ccf72f6b4d2d66e77876a8e4ad1904f8ee
Alex M <57906851+AlexMasood@users.noreply.github.com>
2021-10-18 16:53:24 +0100

    PyGame implementation

--- game.py
+++ game.py
@@ -1,16 +1,21 @@
+from numpy.core.arrayprint import format_float_scientific
 from board import Board as b
 from ai import AI
 from player import Player
+import pygame
 """
 Implementation of reinforcement AI based on code created by MJeremy2017
 https://github.com/MJeremy2017/reinforcement-learning-implementation/blob/master/TicTacToe/tic-tac-toe.ipynb
 """
 class Game:
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2,row = 3,col = 3,pygame = True):
         self.beingPlayed = True
         self.p1 = p1
         self.p2 = p2
-    
+        self.pygame = pygame
+        self.row = row
+        self.col = col
+        self.pixelSize = 100
     """
     Input of the current board object
     Feed both AI based on winner or draw
@@ -27,23 +32,25 @@ class Game:
             self.p2.feedReward(0.5)
     
     """
-    Input of how many rounds will be played
+    Input board row, board column, win number, and of how many rounds will be played
     Finds legal moves, decide the best course of action adds states as a board hash
     Checks board win condition, assigns reward and resets if game is over
     repeats for the second AI
 
     """
-    def aIVsAI(self,rounds = 100):
+    def aIVsAI(self, row, col, winNum, rounds = 100):
         for i in range(rounds):
-            if(i%10000 == 0):
-                print("rounds {}".format(i))
-            boardObj = b()
+            #if(i%1 == 0):
+            print("rounds {}".format(i))
+            boardObj = b(row, col, winNum)
             while (self.beingPlayed):
                 positions = boardObj.getRemainingMoves(boardObj.getBoard())
                 p1Action = self.p1.chooseAction(positions, boardObj.getBoard(), 1)
                 boardObj.move(1,p1Action[0],p1Action[1])
                 boardHash = boardObj.getHash()
+                boardShrinkHash = boardObj.getShrinkHash()
                 self.p1.addState(boardHash)
+                self.p1.addState(boardShrinkHash)
 
                 if((boardObj.checkBoard(1)) or not(boardObj.getRemainingMoves(boardObj.getBoard()))):
                     self.giveReward(boardObj)
@@ -56,7 +63,9 @@ class Game:
                     p2Action = self.p2.chooseAction(positions, boardObj.getBoard(), 2)
                     boardObj.move(2,p2Action[0],p2Action[1])
                     boardHash = boardObj.getHash()
+                    boardShrinkHash = boardObj.getShrinkHash()
                     self.p2.addState(boardHash)
+                    self.p2.addState(boardShrinkHash)
 
                     if((boardObj.checkBoard(2)) or not(boardObj.getRemainingMoves(boardObj.getBoard()))):
                         self.giveReward(boardObj)
@@ -66,11 +75,15 @@ class Game:
                         break
     
     """
+    Inputs of board rows, board columns, and win number
+    Inputs of row column and win number
     Single game of tic-tac-toe with no rewards on win or draw
+    Use function computerFirstGame or humanFirstGame to choose who is first
     """
-    def humanVsAI(self):
-        boardObj = b()
+    def humanVsAI(self, row, col, winNum):
+        boardObj = b(row, col, winNum)
         boardObj.printBoard()
+        print("######")
         while (self.beingPlayed):
             positions = boardObj.getRemainingMoves(boardObj.getBoard())
             p1Action = self.p1.chooseAction(positions, boardObj.getBoard(), 1)
@@ -78,6 +91,7 @@ class Game:
             boardHash = boardObj.getHash()
             self.p1.addState(boardHash)
             boardObj.printBoard()
+            print("######")
 
             if(boardObj.checkBoard(1)):
                 print(self.p1.getName() + " has won")
@@ -94,12 +108,63 @@ class Game:
                 boardHash = boardObj.getHash()
                 self.p2.addState(boardHash)
                 boardObj.printBoard()
+                print("######")
                 if(boardObj.checkBoard(2)):
                     print(self.p2.getName() + " has won")
                     boardObj.reset()
                     break
 
+    def pgHumanvsAI(self, row, col, winNum):
+        boardObj = b(row, col, winNum)
+        loop = True
+        humanTurn = True
+        while loop:
+            for event in pygame.event.get():
+                if (event.type == pygame.QUIT):
+                    loop = False
+                if(pygame.mouse.get_pressed()[0]):#left
+                    if(humanTurn):
+                        pos = pygame.mouse.get_pos()
+
+                        p1Action = [int(pos[0]/self.pixelSize),int(pos[1]/self.pixelSize)]
+                        print(p1Action)
+                        boardObj.move(1,p1Action[0],p1Action[1])
+                        boardHash = boardObj.getHash()
+                        self.p1.addState(boardHash)
+                        humanTurn = not humanTurn
+                        if(boardObj.checkBoard(1)):
+                            print(self.p1.getName() + " has won")
+                            boardObj.reset()
+                            break
+                        elif not(boardObj.getRemainingMoves(boardObj.getBoard())):
+                            print("draw")
+                            boardObj.reset()
+                            break
+            if(not humanTurn):
+
+                positions = boardObj.getRemainingMoves(boardObj.getBoard())
+                p2Action = self.p2.chooseAction(positions, boardObj.getBoard(), 2)
+                boardObj.move(2,p2Action[0],p2Action[1])
+                boardHash = boardObj.getHash()
+                self.p2.addState(boardHash)
+                boardObj.printBoard()
+                print("######")
+                if(boardObj.checkBoard(2)):
+                    print(self.p2.getName() + " has won")
+                    boardObj.reset()
+                    #break
+                humanTurn = not humanTurn
+
+            img = pygame.surfarray.make_surface(boardObj.getBoard())
+            img = pygame.transform.scale(img, (row * self.pixelSize,col * self.pixelSize))
+            #draw to the screen
+            self.screen.fill((0,0,0)) 
+            self.screen.blit(img,(0,0))
+            pygame.display.flip()
+        #pygame.quit()
+
     """
+    Inputs of board rows, board columns, and win number
     whilst the game is being played,
     player 1 move
     check win condition
@@ -107,63 +172,85 @@ class Game:
     else play player 2
     check win condition
     """
-    def humanVsHuman(self,player1, player2):
-        turn = 0
-        firstPlayer = Player(player1)
-        secondPlayer = Player(player2)
-        boardObj = b()
+    def humanVsHuman(self, rowLen, colLen, winNum):
+        boardObj = b(rowLen, colLen, winNum)
         boardObj.printBoard()
         while (self.beingPlayed):
-            firstPlayer.playerMove(boardObj,1)
-            turn += 1
+            positions = boardObj.getRemainingMoves(boardObj.getBoard())
+            p1Action = self.p1.chooseAction(positions, boardObj.getBoard(), 1)
+            boardObj.move(1,p1Action[0],p1Action[1])
+            boardObj.printBoard()
             if(boardObj.checkBoard(1)):
-                print(firstPlayer.getName() + " has won")
+                print(self.p1.getName() + " has won")
                 self.beingPlayed = False
             else:
-                if(turn > 8):
+                if(not boardObj.getRemainingMoves(boardObj.getBoard())):
                     print("Draw")
                     self.beingPlayed = False
                 else:
-                    secondPlayer.playerMove(boardObj,2)
-                    turn += 1
+                    positions = boardObj.getRemainingMoves(boardObj.getBoard())
+                    p2Action = self.p2.chooseAction(positions, boardObj.getBoard(), 2)
+                    boardObj.move(2,p2Action[0],p2Action[1])
+                    boardObj.printBoard()
                     if(boardObj.checkBoard(2)):
-                        print(secondPlayer.getName() + " has won")
+                        print(self.p2.getName() + " has won")
                         self.beingPlayed = False
 
+    def setupScreen(self):
+        pygame.display.set_mode((self.col*self.pixelSize,self.row*self.pixelSize))
+        self.screen = pygame.display.get_surface()
 """
+Inputs of repeated training, board rows, board columns, and win number
 Sets up, trains, and saves the AI
 """
-def trainAI(repitions):
+def trainAI(repitions, row,col,winNum):
     p1 = AI("p1")
     p2 = AI("p2")
     st = Game(p1, p2)
     print("training...")
-    st.aIVsAI(repitions)
-    p1.savePolicy()
-    p2.savePolicy()
+    st.aIVsAI(row,col, winNum, repitions)
+    p1.savePolicy(row,col,winNum)
+    p2.savePolicy(row,col,winNum)
 
 """
+Inputs of board rows, board columns, and win number
 Loads computers AI is first player
 """  
-def computerFirst():
+def computerFirstGame(row,col,winNum):
     p1 = AI("computer", expRate = 0)
     p2 = Player("Human")
-    p1.loadPolicy("policy_p1")
+    p1.loadPolicy(row, col, winNum, "p1")
 
     st = Game(p1,p2)
-    st.humanVsAI()
+    st.humanVsAI(row,col, winNum)
 
 """
+Inputs of board rows, board columns, and win number
 Loads computers AI second player
 """
-def humanFirst():
+def humanFirstGame(row,col,winNum):
+    
+
     p1 = Player("Human")
     p2 = AI("computer", expRate = 0)
-    p2.loadPolicy("policy_p2")
+    p2.loadPolicy(row, col, winNum, "p2")
 
     st = Game(p1,p2)
-    st.humanVsAI() 
+    if(st.pygame):
+        pygame.init()
+        st.setupScreen()
+        st.pgHumanvsAI(row,col, winNum)
+    else:
+        st.humanVsAI(row,col, winNum) 
+"""
+Inputs of board rows, board columns, and win number
+"""
+def humanVsHumanGame(row,col,winNum):
+    p1 = Player("Human1")
+    p2 = Player("Human2")
+    st = Game(p1,p2)
+    st.humanVsHuman(row,col, winNum)
 
-
-trainAI(50000)
-computerFirst()
+#trainAI(1000,15,15,5)
+humanFirstGame(3,3,3)
+#humanVsHumanGame(10,10,5)
